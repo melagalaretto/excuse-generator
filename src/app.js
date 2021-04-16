5d8bdc9d4ffa188b65b322dd4ee9334523c66eba
melagalaretto <moonsea1711@gmail.com>
2021-04-16 18:49:00 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuse();
+  });
 };
+
+function excuse() {
+  let who = ["Mi perro", "Mi madre", "Tu tatarabuelo", "Un pajarito"];
+  let action = ["salió", "comió", "corrió", "terminó"];
+  let what = ["afuera", "en el suelo", "en el pasto", "manzanas"];
+  let when = [
+    "despues del covid",
+    "justo ahora",
+    "ayer",
+    "el otro dia",
+    "hace un mes"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionInd = Math.floor(Math.random() * action.length);
+  let whatInd = Math.floor(Math.random() * what.length);
+  let whenInd = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionInd] +
+    " " +
+    what[whatInd] +
+    " " +
+    when[whenInd]
+  );
+}
